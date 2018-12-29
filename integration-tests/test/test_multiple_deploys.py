46004baa8634aff3757a5e90a1930fdcc9b7b36b
Kent Shikama <kent@kentshikama.com>
2018-12-29 13:27:13 -0800

    Modify multiple deploys test to reproduce unused comm event

--- integration-tests/test/test_multiple_deploys.py
+++ integration-tests/test/test_multiple_deploys.py
@@ -29,6 +29,23 @@ class DeployThread(threading.Thread):
             self.node.deploy(self.contract)
             self.node.propose()
 
+import random
+import os
+
+class DeployRandomThread(threading.Thread):
+    def __init__(self, name, node, count):
+        threading.Thread.__init__(self)
+        self.name = name
+        self.node = node
+        self.relative_paths = self.node.shell_out('sh', '-c', 'ls /opt/docker/examples/*.rho').splitlines()
+        self.count = count
+
+    def run(self):
+        for _ in range(self.count):
+            full_path = os.path.join('/opt/docker/examples', random.choice(self.relative_paths))
+            self.node.deploy(full_path)
+            self.node.propose()
+
 
 BOOTSTRAP_NODE_KEYS = conftest.KeyPair(private_key='80366db5fbb8dad7946f27037422715e4176dda41d582224db87b6c3b783d709', public_key='1cd8bf79a2c1bd0afa160f6cdfeb8597257e48135c9bf5e4823f2875a1492c97')
 BONDED_VALIDATOR_KEY_1 = conftest.KeyPair(private_key='120d42175739387af0264921bb117e4c4c05fbe2ce5410031e8b158c6e414bb5', public_key='02ab69930f74b931209df3ce54e3993674ab3e7c98f715608a5e74048b332821')
@@ -37,7 +54,6 @@ BONDED_VALIDATOR_KEY_3 = conftest.KeyPair(private_key='2b173084083291ac6850cb734
 
 
 def test_multiple_deploys_at_once(command_line_options: CommandLineOptions, docker_client: DockerClient) -> None:
-    contract_path = '/opt/docker/examples/shortfast.rho'
     peers_keypairs = [BONDED_VALIDATOR_KEY_1, BONDED_VALIDATOR_KEY_2, BONDED_VALIDATOR_KEY_3]
     with conftest.testing_context(command_line_options, docker_client, bootstrap_keypair=BOOTSTRAP_NODE_KEYS, peers_keypairs=peers_keypairs) as context:
         with docker_network_with_started_bootstrap(context=context) as bootstrap_node:
@@ -46,7 +62,7 @@ def test_multiple_deploys_at_once(command_line_options: CommandLineOptions, dock
                     with bootstrap_connected_peer(context=context, bootstrap=bootstrap_node, name='bonded-validator-3', keypair=BONDED_VALIDATOR_KEY_3) as no3:
                         wait_for_peers_count_at_least(context, no1, 3)
 
-                        deploy1 = DeployThread("node1", no1, contract_path, 1)
+                        deploy1 = DeployRandomThread("node1", no1, 1)
                         deploy1.start()
 
                         expected_blocks_count = 1
@@ -56,13 +72,13 @@ def test_multiple_deploys_at_once(command_line_options: CommandLineOptions, dock
                             expected_blocks_count,
                         )
 
-                        deploy2 = DeployThread("node2", no2, contract_path, 3)
+                        deploy2 = DeployRandomThread("node2", no2, 15)
                         deploy2.start()
 
-                        deploy3 = DeployThread("node3", no3, contract_path, 3)
+                        deploy3 = DeployRandomThread("node3", no3, 15)
                         deploy3.start()
 
-                        expected_blocks_count = 7
+                        expected_blocks_count = 31
                         wait_for_blocks_count_at_least(
                             context,
                             no1,
